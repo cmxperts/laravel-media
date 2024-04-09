fcd0b77e7f1f3cc8ff7ab36b4f4503528c9fa132
cmxperts <cmxperts@gmail.com>
2024-04-09 10:28:06 +0530

    Update cmx_media.php

--- config/cmx_media.php
+++ config/cmx_media.php
@@ -1,11 +1,25 @@
 <?php
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Configurations
-    |--------------------------------------------------------------------------
-    |
-    */
+    /* you can add your own middleware here */
+    'middleware' => ['web','auth'],
+
+    /* you can set your own model */
+    'model' => CmXperts\MediaManager\Models\Upload::class,
+
+    /* you can set your own table names */
     'table' => 'cmx_uploads',
+
+    /* you can set your route path*/
+    'route_prefix' => 'cmxperts/media',
+
+    /* here you can make menu items visible to specific roles */
+    'use_roles' => false,
+
+    /* If use_roles = true above then must set the table name, primary key and title field to get roles details */
+    'roles_table' => 'roles',
+
+    'roles_pk' => 'id', // primary key of the roles table
+
+    'roles_title_field' => 'name', // display name (field) of the roles table
 ];
